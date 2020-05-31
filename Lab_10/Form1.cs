2f4cd28af26c02cdf9d50cdc73339baa05e5518c
wunan-leo <1421877537@qq.com>
2020-05-31 11:24:19 +0800

    Lab_10

---
+++ Lab_10/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static SqlConnection myconn = new SqlConnection("Initial Catalog=university;Data Source = (local);Integrated Security = True;");
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            string str1="select snum from student";
+            SqlDataAdapter myAdapter = new SqlDataAdapter(str1, myconn);
+            DataSet myDataSet=new DataSet();
+            myAdapter.Fill(myDataSet,"snum");
+            comboBox1.DataSource=myDataSet.Tables["snum"];
+            comboBox1.DisplayMember = "snum";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str2 = "select secnum from sections except "
+               + "select secnum from sc where snum= '" + comboBox1.Text + "' ";
+            SqlDataAdapter myAdapter2 = new SqlDataAdapter(str2, myconn);
+            DataSet myDataSet=new DataSet();
+            myAdapter2.Fill(myDataSet, "secnum");
+            comboBox2.DataSource = myDataSet.Tables["secnum"];
+            comboBox2.DisplayMember = "secnum";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myScore=textBox1.Text;
+            string mySnum=comboBox1.Text;
+            string mySecnum=comboBox2.Text;
+            string cmd="insert into sc values('"+mySnum+"','"+mySecnum+"',"+int.Parse(myScore)+")";
+            SqlCommand myCmd=new SqlCommand(cmd,myconn);
+            myconn.Open();
+            {
+                try
+                {
+                    myCmd.ExecuteNonQuery();
+                    
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                MessageBox.Show("学生成绩添加完成！");
+            }
+            myconn.Close();
+        }
+    }
+}
---
+++ Lab_10/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_10
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        static SqlConnection myconn = new SqlConnection("Initial Catalog=university;Data Source = (local);Integrated Security = True;");
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string str1="select snum from student";
+            SqlDataAdapter myAdapter = new SqlDataAdapter(str1, myconn);
+            DataSet myDataSet = new DataSet();
+            myAdapter.Fill(myDataSet, "snum");
+            comboBox1.DataSource = myDataSet.Tables["snum"];
+            comboBox1.DisplayMember = "snum";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string mySnum=comboBox1.Text;
+            string str2="select sname,count(secnum),avg(score) from student,sc "
+                +"where student.snum=sc.snum and sc.snum='"+mySnum+"'"
+                +" group by sname";
+            SqlCommand myCmd=new SqlCommand(str2,myconn);
+            myconn.Open();
+            {
+                SqlDataReader myReader;
+                myReader=myCmd.ExecuteReader();
+                if (myReader.Read())
+                {
+                    textBox1.Text = myReader.GetValue(0).ToString();
+                    textBox2.Text = myReader.GetValue(1).ToString();
+                    textBox3.Text = myReader.GetValue(2).ToString();
+                }
+            }
+            myconn.Close();
+        }
+    }
+}
